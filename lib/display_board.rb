c0cf6ea65d2b9df6b3bbb915580ae4cfb8c04d1f
Joshua Needham <joshuabneedham@gmail.com>
2017-09-25 19:37:51 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,5 +2,5 @@
 # out the current state.
 def display_board
   puts "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   "
-  puts [" "," "," "," "," "," "," "," "," ",]
+  board = [" "," "," "," "," "," "," "," "," ",]
 end
